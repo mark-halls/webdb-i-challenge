ba76ef9396a59ce3e43856c63b183a4c7246b45c
Mark Halls <mark.halls@gmail.com>
2019-12-09 15:56:40 -0700

    Add logger middleware and import account router

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
-const express = require('express');
+const express = require("express");
 
-const db = require('./data/dbConfig.js');
+const router = require(`./accounts/accounts-router`);
 
 const server = express();
 
-server.use(express.json());
+server.use(logger);
+
+server.use(`/api/accounts`, router);
 
 module.exports = server;
+
+function logger(req, res, next) {
+  console.log(`${req.method} on ${req.originalUrl} at ${new Date(Date.now())}`);
+  next();
+}
